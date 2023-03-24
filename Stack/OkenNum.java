85d4809de61f4c7473a1946fbd2eb698f305716a
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-03-24 16:42:38 +0900

    BubbleSort

---
+++ Stack/OkenNum.java
@@ -0,0 +1,25 @@
+//package Stack;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Stack;
+//import java.util.StringTokenizer;
+//
+//public class OkenNum {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(br.readLine());
+//        int A[] = new int[N+1];
+//        for(int i=0; i<N; i++){
+//            A[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        Stack<Integer> stack = new Stack<>();
+//
+//        for(int i=0; i<N; i++){
+//            while(!stack.isEmpty() || A[i] <)
+//        }
+//    }
+//}
